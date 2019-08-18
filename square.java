7b16684c933c2dfeaeb3a6dcbfc9a583a07b4adc
Sarthak Sharma <49234866+Titanseverus@users.noreply.github.com>
2019-08-18 07:21:40 +0530

    Add files via upload
    square of any no. by java.

---
+++ square.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class square
+   {
+	public static void main(String[]arg)
+   {
+	Scanner S = new Scanner (System.in);
+	int a;
+	System.out.println("no. daal");
+	a = S.nextInt();
+	System.out.print("iska SQuare h "+a*a);
+	
+	}
+	}
