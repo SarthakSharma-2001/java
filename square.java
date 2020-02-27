46f0162d664e2a22c05ab7ff989353994618a7e7
Sarthak Sharma <49234866+SarthakSharma-cs18@users.noreply.github.com>
2020-02-27 18:40:21 +0530

    Update square.java

--- square.java
+++ square.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 class square
-   {
+{
 	public static void main(String[]arg)
-   {
+	{
 	Scanner S = new Scanner (System.in);
 	int a;
 	System.out.println("no. daal");
 	a = S.nextInt();
 	System.out.print("iska SQuare h "+a*a);
-	
 	}
-	}
+}
